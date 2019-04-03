eb6e6cb9ae9ef62d506f3ed3fc05de98b39f6f28
jmgr2007 <masonmillz97@gmail.com>
2019-04-03 14:04:57 -0500

    main() rewrite

---
+++ threads.h
@@ -0,0 +1,14 @@
+#ifndef THREADS_H_
+#define THREADS_H_
+
+extern void fxn0(void);
+extern void life(void);
+extern void heartbeat(void);
+extern void runCommands(void);
+extern void bluetooth(void);
+extern void peidiDC(void);
+extern void calculatePid(void);
+extern void LightSensor(void);
+extern void printQueues(void);
+
+#endif /* UTILS_H_ */
